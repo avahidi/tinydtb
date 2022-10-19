34038ee46a8f94f00e73278b7a323137bd1349eb
Arash Vahidi <arash.vahidi@gmail.com>
2022-10-20 00:03:48 +0200

    simplify fuzzing container and makefile

--- fuzz/lxc.sh
+++ fuzz/lxc.sh
@@ -8,8 +8,8 @@ set -ex
 
 lxc delete -f tinydtb-fuzz || echo "No previous container"
 
-lxc launch images:ubuntu/18.04 tinydtb-fuzz
-lxc config device add tinydtb-fuzz shareddir disk source=`pwd`/.. path=/home/user/tinydtb
+lxc launch images:ubuntu/22.04 tinydtb-fuzz
+lxc config device add tinydtb-fuzz shareddir disk source=`pwd`/.. path=/tinydtb
 
 lxc exec tinydtb-fuzz -- useradd -m -s /bin/bash -G sudo -p "" user
 sleep 5
@@ -17,11 +17,11 @@ sleep 5
 lxc exec tinydtb-fuzz -- su -l user << EOF
 set -ex
 sudo apt update
-sudo apt install -y --no-install-recommends git mg make wget unzip
+sudo apt install -y --no-install-recommends make
 
 # folder is read-only, we need to store our stuff somewhere else
-WORKDIR=/tmp/build make -C tinydtb/fuzz setup
-WORKDIR=/tmp/build make -C tinydtb/fuzz fuzz
+WORKDIR=/tmp/build make -C /tinydtb/fuzz setup
+WORKDIR=/tmp/build make -C /tinydtb/fuzz fuzz
 EOF
 
 
